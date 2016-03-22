7e31d5781f7aff4d2ddeef23e0024a0604616e9e
Paul Schrage <pschrage@norsecorp.com>
2016-03-22 14:21:58 -0700

    Added encoding to hide details in the url

--- _shareit.module.js
+++ _shareit.module.js
@@ -13,7 +13,11 @@ angular.module('shareit', [])
 			var finalUrl = $location.absUrl().indexOf('?') === -1 ? $location.absUrl() + '?shareit=true&' : $location.absUrl() + '&shareit=true&';
 			angular.forEach($scope, function(val, key) {
 				if (key && (typeof val !== "function") && (key.substr(0,1) != '$') && (toIgnore.indexOf(key) === -1) ) {
-					finalUrl += key+'='+val+'&';
+					if ($element.attr('sneaky')) {
+						finalUrl += key+'='+window.btoa(val)+'&';
+					} else {
+						finalUrl += key+'='+val+'&';
+					}
 				}
 			});
 			finalUrl = finalUrl.substr(0, finalUrl.length - 1);
@@ -28,8 +32,11 @@ angular.module('shareit', [])
 			if (urlParams) {
 				for (var i=0; i<urlParams.length; i++) {
 					var keyPair = urlParams[i].split('=');
-					console.log(keyPair);
-					$scope[keyPair[0]] = keyPair[1];
+					if ($element.attr('sneaky')) {
+						$scope[keyPair[0]] = window.atob(keyPair[1]);
+					} else {
+						$scope[keyPair[0]] = keyPair[1];
+					}
 				}
 			}
 		}
